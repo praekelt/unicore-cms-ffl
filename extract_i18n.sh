96e96e3fcd5042918d4a1b283997a739b9c3fedb
Milton Madanda <madandat@gmail.com>
2015-04-28 12:52:01 +0200

    update nepali translations

--- extract_i18n.sh
+++ extract_i18n.sh
@@ -6,7 +6,7 @@ pot-create -o unicorecmsffl/locale/unicorecmsffl.pot unicorecmsffl/
 
 declare -a arr=(
     "eng_GB" "tha_TH" "ind_ID" "swa_TZ" "swa_KE" "spa_ES" "hin_IN" "fre_FR"
-    "por_PT" "ben_IN" "ben_BD" "mar_IN" "tam_IN" "tel_IN" "asm_IN")
+    "por_PT" "ben_IN" "ben_BD" "mar_IN" "tam_IN" "tel_IN" "asm_IN" "nep_NP")
 
 for lang in "${arr[@]}"
 do
